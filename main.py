f2d7650bc02d09d687f57a525611853e669f2783
fofochi1 <alfonsobuzeta@gmail.com>
2022-02-22 10:27:36 -0500

    postgresql

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ from flask_login import UserMixin, LoginManager, login_user, login_required, cur
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-app.secret_key = "I am a secret"
+app.secret_key = os.getenv("SECRET_KEY")
+
+uri = os.getenv("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 # Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 # Gets rid of a warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
